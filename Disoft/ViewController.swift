b3752284146f0b6611ac862a51139b65ff454e1d
QueraltSosaMompel <queraltsosa@hotmail.com>
2020-01-25 21:42:43 +0000

    LoginVC UI was added

--- Disoft/ViewController.swift
+++ Disoft/ViewController.swift
@@ -9,12 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "LoginVC") as! LoginVC
+            self.present(nextVC, animated: false, completion: nil)
+        }
     }
-
-
 }
-

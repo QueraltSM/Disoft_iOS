1b40dba8c905c35db6d78c881f308e7165583ca9
QueraltSosaMompel <queraltsosa@hotmail.com>
2020-01-25 16:29:43 +0000

    Initial Commit

---
+++ Disoft/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Disoft
+//
+//  Created by Queralt Sosa Mompel on 25/1/20.
+//  Copyright © 2020 Queralt Sosa Mompel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

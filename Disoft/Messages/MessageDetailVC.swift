332a56d959eeb68db4e8ee9176a94124c6ea96f7
QueraltSosaMompel <queraltsosa@hotmail.com>
2020-01-27 20:01:12 +0000

    Add MessageDetaiVC UI

--- Disoft/Messages/MessageDetailVC.swift
+++ Disoft/Messages/MessageDetailVC.swift
@@ -15,10 +15,28 @@ class MessageDetailVC: UIViewController {
     var messageTitle : String = ""
     var messageDescription : String = ""
     @IBOutlet weak var navBarTitle: UINavigationItem!
+    @IBOutlet weak var replyBtn: UIButton!
+    @IBOutlet weak var descriptionTxtView: UITextView!
+    @IBOutlet weak var titleLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navBarTitle.title = messageFrom
+        self.titleLbl.text = messageTitle
+        self.descriptionTxtView.text = messageDescription
+        let borderColor : UIColor = UIColor.init(hexString: "#104E01")
+        descriptionTxtView.layer.borderWidth = 2
+        descriptionTxtView.layer.borderColor = borderColor.cgColor
+        descriptionTxtView.layer.cornerRadius = 5.0
+        titleLbl.layer.borderWidth = 2
+        titleLbl.layer.borderColor = borderColor.cgColor
+        titleLbl.layer.cornerRadius = 2
+        replyBtn.layer.cornerRadius = replyBtn.bounds.height / 2
+        let roundPath = UIBezierPath(roundedRect: replyBtn.bounds, cornerRadius: replyBtn.bounds.height / 2)
+        let maskLayer = CAShapeLayer()
+        maskLayer.path = roundPath.cgPath
+        replyBtn.layer.mask = maskLayer
+        replyBtn.backgroundColor = UIColor.init(hexString: "#B1FF9E")
     }
     
     @IBAction func back(_ sender: Any) {
